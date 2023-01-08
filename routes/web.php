4655f3d5e87cc7c7b7962905dd2882dfeb453d0d
LelouchLamperougee <pavel.bv2003@gmail.com>
2023-01-08 18:57:59 +0300

    Add App controller and route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AppController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('{page}', [AppController::class, 'app'])->where('page', '.*');
 
 Auth::routes();
 
