39747517c8890326ec41da56d90c5ec679f1b7d9
LelouchLamperougee <pavel.bv2003@gmail.com>
2023-01-10 15:26:31 +0300

    login frontend

--- resources/js/components/content/auth/Login.vue
+++ resources/js/components/content/auth/Login.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-        login
+    <div class="login">
+        <div class="login-title">
+            <div><h2>Login:</h2></div>
+        </div>
+        <div class="login-content">
+            <input type="email" placeholder="Email">
+            <input type="password" placeholder="Password">
+            <input class="login-submit" type="submit" value="Sign in">
+        </div>
     </div>
 </template>
 
@@ -11,5 +18,19 @@ export default {
 </script>
 
 <style scoped>
+    .login{
+        display: flex;
+        flex-direction: column;
+    }
 
+    .login-content input{
+        margin: 10px;
+        width: 200px;
+    }
+
+    .login-submit{
+        background-color: lightseagreen;
+        border: none;
+        height: 30px;
+    }
 </style>
