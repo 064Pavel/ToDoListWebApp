31c0d5f26bd6b373087b485601c52ceb23d16d84
LelouchLamperougee <pavel.bv2003@gmail.com>
2023-01-12 15:56:28 +0300

    adjusting the frontend task

--- resources/js/components/content/tasks/Index.vue
+++ resources/js/components/content/tasks/Index.vue
@@ -1,10 +1,25 @@
 <template>
-    <div v-if="token">
-        <p><a @click.prevent="logout">Logout</a></p>
+    <div class="task" v-for="task in tasks">
+        <div class="task_info">
+            <div class="task_info_section_1">
+
+                <div class="task_info_delete">
+                    <input type="checkbox">
+                </div>
+
+                <div class="task_info_title">
+                    <strong class="task_info_title_text">{{ task.title }}</strong>
+                </div>
+
+            </div>
+
+            <div class="task_info_section_2">
+                <strong>+</strong>
+            </div>
+
+        </div>
     </div>
-    <div v-for="task in tasks">
-        {{task.title}}
-    </div>
+
 </template>
 
 <script>
@@ -13,41 +28,74 @@ import router from "../../../router";
 export default {
     name: "Index",
 
-    data(){
-        return{
-            token: null,
+    data() {
+        return {
             tasks: []
         }
     },
 
     mounted() {
         this.getTasks()
-        this.getToken()
     },
 
-    methods:{
-        getTasks(){
+    methods: {
+        getTasks() {
             axios.get('/api/tasks')
                 .then(response => {
                     this.tasks = response.data.data
                 })
         },
 
-        getToken(){
-            this.token = localStorage.getItem('x_xsrf_token')
-        },
 
-        logout(){
-            axios.post('/logout')
-                .then(response => {
-                    localStorage.removeItem('x_xsrf_token')
-                    router.push({name: 'user.login'})
-                })
-        }
     }
 }
 </script>
 
 <style scoped>
 
+.task{
+    background-color: white;
+    min-height: 4vh;
+    text-align: left;
+    width: 100%;
+    margin-bottom: 2vh;
+}
+
+.not{
+    border-left: 8px solid green;
+}
+
+.low{
+    border-left: 8px solid blue;
+}
+
+.middle{
+    border-left: 8px solid yellow;
+}
+
+.high{
+    border-left: 8px solid red;
+}
+
+
+.task_info_section_1{
+    display: flex;
+    margin-left: 2vh;
+}
+
+.task_info_section_2{
+    display: flex;
+    margin-right: 2vh;
+}
+
+.task_info{
+    display: flex;
+    padding-top: 1vh;;
+    justify-content: space-between;
+}
+
+.task_info div{
+
+    margin-right: 2vh;
+}
 </style>
--- resources/js/components/header/HeaderComponent.vue
+++ resources/js/components/header/HeaderComponent.vue
@@ -1,15 +1,51 @@
 <template>
     <div class="header">
         <div class="header-content">
-            <div class="header-title"><h1>ToDo List WebApp</h1></div>
+            <div class="header-section-1">
+
+                <div class="header-title"><h1>ToDo List WebApp</h1></div>
+
+                <div class="logout" v-if="token">
+                    <p><a @click.prevent="logout">Logout</a></p>
+                </div>
+            </div>
+
             <div class="header-description"><h3>Skillfully plan your day. Manage your to-do list online.</h3></div>
         </div>
+
     </div>
 </template>
 
 <script>
+import router from "../../router";
+
 export default {
-    name: "Header"
+    name: "Header",
+
+    data(){
+        return{
+            token: null
+        }
+    },
+
+    mounted() {
+        this.getToken()
+    },
+
+    methods: {
+        getToken(){
+            this.token = localStorage.getItem('x_xsrf_token')
+        },
+
+        logout(){
+            axios.post('/logout')
+                .then(response => {
+                    localStorage.removeItem('x_xsrf_token')
+                    router.push({name: 'user.login'})
+                })
+        }
+
+    }
 }
 </script>
 
@@ -25,5 +61,23 @@ export default {
     .header-content{
         text-align: center;
     }
+
+    .header-section-1{
+        position: relative;
+        display: flex;
+        left: 50%;
+        transform: translate(-25%, 0);
+    }
+
+    .header-title{
+        margin-right: 25px;
+    }
+
+    .logout{
+        position: relative;
+        margin-top: 14px;
+        font-weight: bold;
+    }
+
 
 </style>
